b3bf449b7c0735e9b46626ab5ca7c40979b1ef58
cas3800 <cas.38@yandex.ru>
2020-12-22 19:27:46 +0800

    EF Update

---
+++ Device.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AppServer
+{
+    public partial class Device
+    {
+        public int PlayerId { get; set; }
+        public string DeviceId { get; set; }
+
+        public virtual Player Player { get; set; }
+    }
+}
