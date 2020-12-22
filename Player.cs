f82da8551614a4fd1f29b422421f59826b579292
cas3800 <cas.38@yandex.ru>
2020-12-22 19:27:46 +0800

    EF Update

---
+++ Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace AppServer
+{
+    public partial class Player
+    {
+        public int PlayerId { get; set; }
+        public bool? AllowDid { get; set; }
+        public bool AllowPass { get; set; }
+        public string Token { get; set; }
+    }
+}
