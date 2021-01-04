8ee38116e75f3d87d330e3d272083bd4b5c496ca
cas3800 <cas.38@yandex.ru>
2021-01-04 18:26:06 +0800

    Регистрация по адресу электронной почты

--- RegService.cs
+++ RegService.cs
@@ -35,16 +35,44 @@ namespace AppServer
                 var command = new MySqlCommand("_NewPlayerByDevice", connection) { CommandType = CommandType.StoredProcedure };
                 command.Parameters.AddWithValue("Device", DtokenPayload["UI"]);
                 command.Parameters.AddWithValue("LogInfo", JsonGsTools.ObjectToJson(DtokenPayload));
-                command.ExecuteNonQueryAsync();
+                var result = new MySqlParameter("Result", MySqlDbType.Int32) { Direction = ParameterDirection.Output };
+                command.Parameters.Add(result);
+                command.ExecuteNonQuery();
+                if ((int)command.Parameters["Result"].Value == 0)
+                    return (new Message(new Dictionary<string, string>{ {"Result", "OK" } })).ToJson();
+                else
+                    return (new Message(new Dictionary<string, string>{ {"Result", "Duplicated device ID" } })).ToJson();
             }
-            return (new Message(new Dictionary<string, string>
-                            { {"Result", "OK" } }
-            )).ToJson();
+            else
+            return (new Message(new Dictionary<string, string>{ {"Result", "Invalid device ID" } })).ToJson();
         }
 
         private static string RegByEmail(Message msg, HttpContext context)
         {
-            return "";
+            var DtokenPayload = JsonWebToken.DecodeToObject<Dictionary<string, string>>(msg.Data["Dtoken"], "", false);
+            DtokenPayload.Add("IP", context.Connection.RemoteIpAddress.ToString());
+            var email = JsonWebToken.DecodeToObject<Dictionary<string, string>>(msg.Data["Etoken"], "", false)["Email"].Trim().ToLower();
+
+            if (JsonGsTools.IsValidEmailAddress(email))
+            {
+                using var connection = new MySqlConnection(Startup.AppConfiguration.GetConnectionString("Auth"));
+                connection.Open();
+
+                var command = new MySqlCommand("_NewPlayerByEmail", connection) { CommandType = CommandType.StoredProcedure };
+                command.Parameters.AddWithValue("Device", DtokenPayload["UI"]);
+                command.Parameters.AddWithValue("Email", email);
+                command.Parameters.AddWithValue("Etoken", msg.Data["Etoken"]);
+                command.Parameters.AddWithValue("LogInfo", JsonGsTools.ObjectToJson(DtokenPayload));
+                var result = new MySqlParameter("Result", MySqlDbType.Int32) { Direction = ParameterDirection.Output };
+                command.Parameters.Add(result);
+                command.ExecuteNonQuery();
+                if ((int)command.Parameters["Result"].Value == 0)
+                    return (new Message(new Dictionary<string, string> { { "Result", "OK" } })).ToJson();
+                else
+                    return (new Message(new Dictionary<string, string> { { "Result", "Duplicated Email" } })).ToJson();
+            }
+            else
+                return (new Message(new Dictionary<string, string> { { "Result", "Invalid Email" } })).ToJson();
         }
 
         private static string ChangePassword(Message msg, HttpContext context)
